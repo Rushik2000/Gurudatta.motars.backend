4f7b07fb33ae7034adfa7f112dd4b4cb3c9b861f
Rushi <kalerushi200@gmail.com>
2025-10-24 15:45:09 +0530

    updated origin URL for frontend

--- src/main/java/com/gurudatta/motars/backend/controller/CustomerController.java
+++ src/main/java/com/gurudatta/motars/backend/controller/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173") // Allow frontend access
+@CrossOrigin(origins = "http://localhost:3000") // Allow frontend access
 public class CustomerController {
     @Autowired
     CustomerService customerService;
